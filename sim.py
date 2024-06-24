e67a3a50c0e883fc6681e17f1f00ebd3e84e9e4c
Tobias Kappé <tobias@kap.pe>
2024-06-24 13:30:39 +0200

    Implement TriState element.

--- sim.py
+++ sim.py
@@ -1584,3 +1584,40 @@ class TwoComplementElement(BitArithmeticElement):
     def resolve(self):
         self.output1.value = self.twos_complement(self.inp1.value)
         yield self.output1
+
+
+@Circuit.add_impl('TriState')
+class TriStateElement(Element):
+    @singledispatchmethod
+    def __init__(
+        self,
+        inp1: Node,
+        output1: Node,
+        state: Node,
+        **kwargs
+    ):
+        self.inp1 = inp1
+        self.output1 = output1
+        self.state = state
+
+        super().__init__([self.inp1, self.output1], **kwargs)
+
+    @__init__.register
+    def load(self, raw_element: dict, **kwargs):
+        super().__init__(raw_element, **kwargs)
+        self.bitwidth = self.params[1]
+
+    def is_resolvable(self):
+        if self.inp1.value is None:
+            return False
+        if None in self.inp1.value:
+            return False
+        if self.state.value is None:
+            return False
+        if self.state.value[0] is False:
+            return False
+        return True
+
+    def resolve(self):
+        self.output1.value = self.inp1.value
+        yield self.output1
